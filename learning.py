f56255fc71cbae0f73c93a2db8068d24a4d113f6
HoangNguyen5533 <nvanhoang5397@gmail.com>
2021-09-07 19:54:24 +0700

    main

--- learning.py
+++ learning.py
@@ -1,4 +1,4 @@
-filePath = 'messi5.jpg'
+# filePath = 'messi5.jpg'
 '''Mouse event'''
 # import cv2 as cv
 # import numpy as np
@@ -154,23 +154,68 @@ filePath = 'messi5.jpg'
 #     e2 = cv2.getTickCount()
 #     t = (e2 - e1)/cv2.getTickFrequency()
 #     print(t)
+'''Color Object tracking'''
+# import cv2 as cv
+# import numpy as np
+# def nothing(x):
+#     pass
+# cv.namedWindow('Bluelow')
+# cv.namedWindow('Blueup')
+# cv.createTrackbar('r_l','Bluelow',0,255,nothing)
+# cv.createTrackbar('g_l','Bluelow',0,255,nothing)
+# cv.createTrackbar('b_l','Bluelow',0,255,nothing)
+
+# cv.createTrackbar('r_u','Blueup',0,255,nothing)
+# cv.createTrackbar('g_u','Blueup',0,255,nothing)
+# cv.createTrackbar('b_u','Blueup',0,255,nothing)
+# cap = cv.VideoCapture(0)
+# while True:
+#     ret,frame = cap.read()
+#     gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+#     hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
+#     r_l = cv.getTrackbarPos('r_l','Bluelow')
+#     g_l = cv.getTrackbarPos('g_l','Bluelow')
+#     b_l = cv.getTrackbarPos('b_l','Bluelow')
+
+#     r_u = cv.getTrackbarPos('r_u','Blueup')
+#     g_u = cv.getTrackbarPos('g_u','Blueup')
+#     b_u = cv.getTrackbarPos('b_u','Blueup')
+#     low_bl = np.array([60,100,100])
+#     up_bl = np.array([70,255,255])
+#     mask1 = cv.inRange(hsv,low_bl,up_bl)
+#     res = cv.bitwise_and(frame,frame,mask = mask1)
+
+#     cv.imshow('camera',frame)
+#     cv.imshow('mask',mask1)
+#     cv.imshow('res',res)
+#     cv.imshow('gray',hsv)
+#     if cv.waitKey(1) & 0xff == ord('q'):
+#         break
+# cv.destroyAllWindows
+'''find value HSV'''
+# import cv2 as cv
+# import numpy as np
+# green = np.uint8([[[0,0,255]]])
+# hsv_green = cv.cvtColor(green,cv.COLOR_BGR2HSV)
+# print(hsv_green)
+'''Threshold'''
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
+img = cv.imread('messi5.jpg',0)
+ret,thresh1 = cv.threshold(img,125,255,cv.THRESH_BINARY)
+ret,thresh2 = cv.threshold(img,125,255,cv.THRESH_BINARY_INV)
+ret,thresh3 = cv.threshold(img,125,255,cv.THRESH_TRUNC)
+ret,thresh4 = cv.threshold(img,125,255,cv.THRESH_TOZERO)
+ret,thresh5 = cv.threshold(img,125,255,cv.THRESH_TOZERO_INV)
 
-cap = cv.VideoCapture(0)
-while True:
-    ret,frame = cap.read()
-    gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-    hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
-    low_bl = np.array([70,50,50])
-    up_bl = np.array([150,255,255])
-    mask1 = cv.inRange(hsv,low_bl,up_bl)
-    res = cv.bitwise_and(frame,frame,mask = mask1)
+titles = ['img','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+image = [img,thresh1,thresh2,thresh3,thresh4,thresh5]
 
-    cv.imshow('camera',frame)
-    cv.imshow('mask',mask1)
-    cv.imshow('res',res)
-    cv.imshow('gray',hsv)
-    if cv.waitKey(1) & 0xff == ord('q'):
-        break
-cv.destroyAllWindows
+for i in range(6):
+    plt.subplot(2,3,i+1)
+    plt.imshow(image[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([])
+    plt.yticks([])
+plt.show()
